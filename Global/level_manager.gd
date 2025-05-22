d07434ca19a377bb5455e8d29a95ecae73052f11
Krapivin Stepan <xevinbox@yandex.ru>
2025-05-22 15:52:15 +0300

    добавил get_color_with_type()

--- Global/level_manager.gd
+++ Global/level_manager.gd
@@ -5,6 +5,23 @@ var current_level_colors: Array
 var is_prev_gameover: bool
 var current_level_data: Array
 
+enum BlockType {
+	COMMON,
+	KEY_1,
+	KEY_2,
+	KEY_3,
+	KEY_4,
+	KEY_5,
+	LOCK_1,
+	LOCK_2,
+	LOCK_3,
+	LOCK_4,
+	LOCK_5,
+	LIVE,
+	ICE,
+	ROCKET,
+}
+
 func _set_current_level(value) -> void:
 	prints("set cur lvl", value)
 	current_level = value
@@ -15,6 +32,68 @@ func get_random_color():
 	var random_index = randi() % keys.size()
 	var random_key = keys[random_index]
 	return LevelData.COLORS[random_key]
+
+
+# TODO: протестировать
+func get_color_with_type(color: int) -> Dictionary:
+	var _normalized_color = color
+	var block_type: BlockType = BlockType.COMMON
+
+	# Это ключ?
+	if color >= 20 and color <= 67:
+		if color <= 27:
+			_normalized_color = color - 10
+			block_type = BlockType.KEY_1
+		elif color <= 37:
+			_normalized_color = color - 20
+			block_type = BlockType.KEY_2
+		elif color <= 47:
+			_normalized_color = color - 30
+			block_type = BlockType.KEY_3
+		elif color <= 57:
+			_normalized_color = color - 40
+			block_type = BlockType.KEY_4
+		else:
+			_normalized_color = color - 50
+			block_type = BlockType.KEY_5
+
+	# Это замок?
+	elif color >= 210 and color <= 617:
+			if color <= 217:
+				_normalized_color = color - 200
+				block_type = BlockType.LOCK_1
+			elif color <= 317:
+				_normalized_color = color - 300
+				block_type = BlockType.LOCK_2
+			elif color <= 417:
+				_normalized_color = color - 400
+				block_type = BlockType.LOCK_3
+			elif color <= 517:
+				_normalized_color = color - 500
+				block_type = BlockType.LOCK_4
+			else:
+				_normalized_color = color - 600
+				block_type = BlockType.LOCK_5
+
+	# Живой куб?
+	elif color >= 1010 and color <= 1017:
+		_normalized_color = color - 1000
+		block_type = BlockType.LIVE
+
+	# Замороженный куб
+	elif color >= 2010 and color <= 2017:
+		_normalized_color = color - 2000
+		block_type = BlockType.ICE
+
+	# Ракета
+	elif color >= 3010 and color <= 3017:
+		_normalized_color = color - 3000
+		block_type = BlockType.ROCKET
+
+	return {
+		"color": _normalized_color,
+		"type": block_type
+	}
 
 
 func get_cell(pos: Vector2i) -> Array:
