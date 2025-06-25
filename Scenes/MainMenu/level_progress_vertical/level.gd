fee8c612228725ae2ea53c1615f440f521ab107b
Krapivin Stepan <xevinbox@yandex.ru>
2025-06-25 14:11:19 +0300

    новый префаб прогресса на главном экране

---
+++ Scenes/MainMenu/level_progress_vertical/level.gd
@@ -0,0 +1,54 @@
+extends Control
+
+@export_enum("normal", "hard", "super_hard") var type: String
+@export var is_shadowed: bool
+@export var is_active: bool
+@export var level: int
+
+@export var normal_bg: Texture2D
+@export var hard_bg: Texture2D
+@export var super_hard_bg: Texture2D
+@export var reached_line: Texture2D
+@export var disabled_line: Texture2D
+
+
+@onready var background: TextureRect = $Background
+@onready var level_label: Label = $Background/LevelLabel
+@onready var lock: TextureRect = $Background/Lock
+@onready var skull_bg: TextureRect = $Background/Skull_bg
+@onready var line: NinePatchRect = $Line
+
+
+func _ready() -> void:
+	_update_ui()
+
+
+func _update_ui() -> void:
+	if not background:
+		return
+
+	level_label.text = str(level)
+	skull_bg.hide()
+
+	lock.visible = not is_active
+	if not is_active:
+		line.texture = disabled_line
+	else:
+		line.texture = reached_line
+
+	match type:
+		"normal":
+			background.texture = normal_bg
+			background.self_modulate = Color(0, 1, 0)
+		"hard":
+			background.texture = hard_bg
+			background.self_modulate = Color(1,1,1,1)
+		"super_hard":
+			background.texture = super_hard_bg
+			skull_bg.show()
+			if is_active:
+				skull_bg.self_modulate = Color(1, 0, 0)
+
+	if is_shadowed:
+		background.texture = normal_bg
+		background.self_modulate = Color(1,1,1,1)
---
+++ Scenes/MainMenu/level_progress_vertical/level_progress_vertical.gd
@@ -0,0 +1,35 @@
+extends Control
+
+@onready var level_0: Control = $Level0
+@onready var level_1: Control = $Level1
+@onready var level_2: Control = $Level2
+@onready var level_3: Control = $Level3
+
+@onready var levels = [
+	level_0,
+	level_1,
+	level_2,
+	level_3,
+]
+
+
+func _ready() -> void:
+	_update_ui()
+
+
+func _update_ui() -> void:
+	var _cur_level_idx = LevelManager.current_level
+
+	for node in levels:
+		node.level = _cur_level_idx + 1
+
+		if _cur_level_idx in LevelData.hard_levels:
+			node.type = "hard"
+		elif _cur_level_idx in LevelData.super_hard_levels:
+			node.type = "super_hard"
+		else:
+			node.type = "normal"
+
+		node._update_ui()
+
+		_cur_level_idx += 1
